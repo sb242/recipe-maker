efe2c58f795c2e69557488d3c826bcb1f3909bfc
Spencer <spencerb242@proton.me>
2023-01-06 20:12:57 -0700

    created navbar component and added styling

--- src/App.js
+++ src/App.js
@@ -6,11 +6,13 @@ import Home from "./pages/home/Home";
 import Create from "./pages/create/Create";
 import Recipe from "./pages/recipe/Recipe";
 import Search from "./pages/search/Search";
+import Navbar from "./ components/Navbar";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
+        <Navbar />
         <Switch>
           <Route exact path="/">
             <Home />
