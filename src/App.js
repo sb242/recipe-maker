a088115a3ad11063ad5206f0aac60a86aa662a0f
Spencer <spencerb242@proton.me>
2023-01-06 20:03:28 -0700

    setup react-router routing for pages

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
-import './App.css'
+import "./App.css";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+
+//page components
+import Home from "./pages/home/Home";
+import Create from "./pages/create/Create";
+import Recipe from "./pages/recipe/Recipe";
+import Search from "./pages/search/Search";
 
 function App() {
   return (
     <div className="App">
-      
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/create">
+            <Create />
+          </Route>
+          <Route path="/recipes/:id">
+            <Recipe />
+          </Route>
+          <Route path="/search">
+            <Search />
+          </Route>
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
 
-export default App
+export default App;
