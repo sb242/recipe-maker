9bbee57d1ab42778b676885727581ab186927031
Spencer <spencerb242@proton.me>
2023-01-09 14:44:23 -0700

    changed the way firestore data was being called so it now sends a new snapshot everytime the database is modified.

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -11,10 +11,8 @@ export default function Home() {
 
   useEffect(() => {
     setIsPending(true);
-    projectFirestore
-      .collection("recipes")
-      .get()
-      .then((snapshot) => {
+    const unsub = projectFirestore.collection("recipes").onSnapshot(
+      (snapshot) => {
         if (snapshot.empty) {
           setError("No recipes to load");
           setIsPending(false);
@@ -26,11 +24,13 @@ export default function Home() {
           setData(results);
           setIsPending(false);
         }
-      })
-      .catch((err) => {
+      },
+      (err) => {
         setError(err.message);
         setIsPending(false);
-      });
+      }
+    );
+    return () => unsub();
   }, []);
 
   return (
