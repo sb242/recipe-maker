be87ef4d13fe38a1300063b46cfc3e4d98b65bce
Spencer <spencerb242@proton.me>
2023-01-09 12:07:42 -0700

    updated themeselector component to handle multiple colors

--- src/ components/ThemeSelector.js
+++ src/ components/ThemeSelector.js
@@ -1,2 +1,24 @@
 //styles
 import "./ThemeSelector.css";
+
+import { useTheme } from "../hooks/useContext";
+
+export default function ThemeSelector() {
+  const { changeColor } = useTheme();
+
+  const themeColors = ["#58249c", "#249c6b", "#b70233"];
+
+  return (
+    <div className="theme-selector">
+      <div className="theme-buttons">
+        {themeColors.map((color) => (
+          <div
+            key={color}
+            onClick={() => changeColor(color)}
+            style={{ background: color }}
+          />
+        ))}
+      </div>
+    </div>
+  );
+}
