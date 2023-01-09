0e79b358608144d23a8a3e9513e5f66a5e7aa02c
Spencer <spencerb242@proton.me>
2023-01-09 14:33:19 -0700

    updated create component to use firebase to create a new document in the recipes collection when submitted

--- src/ components/RecipeList.js
+++ src/ components/RecipeList.js
@@ -3,6 +3,8 @@ import "./RecipeList.css";
 import { Link } from "react-router-dom";
 import React from "react";
 import { useTheme } from "../hooks/useContext";
+import Trashcan from "../assets/trashcan.svg";
+import { projectFirestore } from "../firebase/config";
 
 export default function RecipeList({ recipes }) {
   const { mode } = useTheme();
@@ -10,6 +12,10 @@ export default function RecipeList({ recipes }) {
   if (recipes.length === 0) {
     return <div className="error">No Recipes Found</div>;
   }
+
+  const handleClick = (id) => {
+    projectFirestore.collection("recipes").doc(id).delete();
+  };
 
   return (
     <div className="recipe-list">
@@ -19,6 +25,11 @@ export default function RecipeList({ recipes }) {
           <p>{recipe.cookingTime} to make</p>
           <div>{recipe.method.substring(0, 100)}...</div>
           <Link to={`/recipes/${recipe.id}`}>Cook This</Link>
+          <img
+            className="delete"
+            src={Trashcan}
+            onClick={() => handleClick(recipe.id)}
+          />
         </div>
       ))}
     </div>
